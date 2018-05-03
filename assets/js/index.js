a6ab35de6c59e1eef058cbd27b0fc70134df119f
userJapa <willian.hsimabukuro@gmail.com>
2018-05-03 15:50:14 -0300

    added fade out alert

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 const alerts = document.querySelectorAll('.fa-times-circle')
+const alertsFade = document.querySelectorAll('.alert-fade')
+
+const options = { attributes: true }
 
 function getAlert(el) {
   if (el.classList.contains('alert')) return el
@@ -12,8 +15,7 @@ function checkShowing(el) {
 
 alerts.forEach(el => el.addEventListener('click', () => {
   const father = getAlert(el)
-  if (checkShowing(father)) father.classList.add('alert_none')
-  else father.classList.remove('alert_none')
+  father.classList.toggle('alert_none')
 }))
 
 function show (btn) {
@@ -22,3 +24,28 @@ function show (btn) {
     if (!checkShowing(el)) el.classList.remove(`alert_none`)
   }
 }
+
+const onClassChange = new Event(`classChange`)
+
+function createNewObserver() {
+  const observer = new MutationObserver(mutationList => {
+    mutationList[0].target.dispatchEvent(onClassChange)
+    observer.disconnect()
+  })
+  return observer
+}
+
+alertsFade.forEach(el => {
+  const mutation = createNewObserver()
+  mutation.observe(el, options)
+})
+
+alertsFade.forEach(el => el.addEventListener('classChange', ev => {
+  const time = parseInt(ev.target.getAttribute('data-fade-time')) * 1000
+  console.log('Class Changed!', ev)
+  setTimeout(() => {
+    ev.target.classList.toggle('alert_none')
+    const mutation = createNewObserver()
+    mutation.observe(ev.target, options)
+  }, time)
+}))
